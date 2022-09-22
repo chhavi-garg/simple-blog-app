b16092a0052c7e08638e65f4e7013ebad9132c25
Chhavi Garg <chhavu.g@gmail.com>
2022-09-23 02:11:39 +0530

    useEffect

--- blogapp/src/components/Home.js
+++ blogapp/src/components/Home.js
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
 
     // let name = 'Chhavi';
-
-    const [name,setName] = useState('Chhavi');
+    // const [name,setName] = useState('Chhavi');
     const [age,setAge] = useState(25);
 
+    const [blogs,setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ]);
+
+      const [name,setName] = useState('mario');
+      const handleDelete =(id) =>{
+        const newBlogs = blogs.filter(blog =>blog.id!==id);
+        setBlogs(newBlogs);
+      }
+
+      useEffect(()=>{
+        console.log('use effect ran');
+        console.log(blogs);
+        console.log(name);
+      },[name]);
     const handleClick = ()=>
     {
         // name = 'Someone';
@@ -17,6 +34,10 @@ const Home = () => {
             <h2>HomePage</h2>
             <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
+            <BlogList blogs={blogs} title="All Blogs ! " handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title="Mario's Blogs ! " />
+            <button onClick={()=> setName('chhavi')}>change name</button>
+            <p>{name}</p>
         </div>
       );
 }
