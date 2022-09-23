f1fe60dfbffce9ca89cf35ace86720fcb1c869d0
Chhavi Garg <chhavu.g@gmail.com>
2022-09-23 19:16:18 +0530

    useFetch

--- blogapp/src/components/Home.js
+++ blogapp/src/components/Home.js
@@ -1,45 +1,17 @@
-import { useState,useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    // let name = 'Chhavi';
-    // const [name,setName] = useState('Chhavi');
-    const [age,setAge] = useState(25);
-
-    const [blogs,setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
-
-      const [name,setName] = useState('mario');
-      const handleDelete =(id) =>{
-        const newBlogs = blogs.filter(blog =>blog.id!==id);
-        setBlogs(newBlogs);
-      }
-
-      useEffect(()=>{
-        console.log('use effect ran');
-        console.log(blogs);
-        console.log(name);
-      },[name]);
-    const handleClick = ()=>
-    {
-        // name = 'Someone';
-        setName('Someone');
-        setAge(30);
-    }
-    return (
-        <div className="home">
-            <h2>HomePage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
-            <BlogList blogs={blogs} title="All Blogs ! " handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title="Mario's Blogs ! " />
-            <button onClick={()=> setName('chhavi')}>change name</button>
-            <p>{name}</p>
-        </div>
-      );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
 export default Home;
